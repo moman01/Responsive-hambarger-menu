99a43b669c7c93bfbf7e3b5619536fba6edae456
Minhajul Abedin <learnermoman@gmail.com>
2024-06-21 16:31:53 +0600

    delete some data

--- chat.js
+++
@@ -1,6 +0,0 @@
-// script.js
-document.getElementById("menu-icon").addEventListener("click", function () {
-  var navbar = document.getElementById("navbar");
-  navbar.classList.toggle("responsive");
-  this.classList.toggle("active");
-});
